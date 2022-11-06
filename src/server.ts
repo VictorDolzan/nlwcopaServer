5c56fda57980151dca0460ef6dd2e50f9f6f453a
Victor Dolsan <victor.freitas@mazzafc.tech>
2022-11-06 19:17:08 -0300

    Criada validação de usuário usando o JWT

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Fastify, {fastify} from 'fastify';
 import cors from '@fastify/cors';
+import jwt from "@fastify/jwt";
 
 import {poolRoutes} from "./routes/pool";
 import {userRoutes} from "./routes/user";
@@ -15,6 +16,11 @@ async function bootstrap(){
     await fastify.register(cors, {
         origin: true,
     });
+
+    //Em produção isso precisa ser uma variável ambiente
+    await fastify.register(jwt, {
+        secret: 'nlwcopa',
+    })
 
     await fastify.register(authRoutes);
     await fastify.register(poolRoutes);
